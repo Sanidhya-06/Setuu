a95b9c0827bf0e7e3369dcedfc40412909558b16
Sanidhya-06 <sanidhya354@gmail.com>
2026-04-26 14:07:34 +0530

    initial monorepo setup with two apps

---
+++ apps/setu_ngo/android/app/src/main/kotlin/com/example/setu_ngo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.setu_ngo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ apps/setu_partners/android/app/src/main/kotlin/com/example/setu_partners/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.setu_partners
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
